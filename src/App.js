2110b2db97f0a8da96c5267fd0e4fbe9e39220b0
Areh Tunde <arehtunde96@gmail.com>
2020-11-18 11:34:14 +0100

    solution to courseinfo exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+const Header = ({course}) => <h1>{course}</h1>;
 
-function App() {
+const Content = ({details}) => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    details.map(({id, part, exercises}) => <p key={id}>{part} {exercises}</p>)
+  )
+};
+
+const Total = ({total}) => {
+   const add = total.reduce((prev, cur) => {
+     return prev + cur.exercises;
+    }, 0);
+
+   return (
+    <p>Number of exercises {add}</p>
+   )
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const details = [
+    {
+      id: 1,
+      part: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      id: 2,
+      part: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      id: 3,
+      part: 'State of a component',
+      exercises: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content details={details} />
+      <Total total={details}/>
     </div>
-  );
+  )
 }
 
 export default App;
