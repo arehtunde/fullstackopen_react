c8e5ad37a6364de993cae65a0a82a1814c3841b2
Areh Tunde <arehtunde96@gmail.com>
2020-12-02 14:52:50 +0100

    added db.json file

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import DisplayContact from './components/DisplayContact'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newDetails, setnewDetails] = useState({
     name: '', number: ''
   })
